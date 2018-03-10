c7893d512ce14e2c048a29659824588f520535f8
Jonathan <jonathan@drumeo.com>
2018-03-09 19:00:18 -0800

    update composer.json
    install couldn't run with other versions specified before

---
+++ tests/Acceptance/SoundsliceTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Railroad\Soundslice\Tests\Acceptance;
+
+
+use Orchestra\Testbench\TestCase;
+
+class SoundsliceTest extends TestCase
+{
+
+
+    public function test_create()
+    {
+//        $this->assert
+    }
+}
