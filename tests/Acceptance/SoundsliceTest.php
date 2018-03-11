2c427a3b14a6fa50d8da7c59f6221f84e6842b04
Jonathan <jonathan@drumeo.com>
2018-03-10 19:29:26 -0800

    test - utilities copied from test in railcontent repo

--- tests/Acceptance/SoundsliceTest.php
+++ tests/Acceptance/SoundsliceTest.php
@@ -7,10 +7,70 @@ use Orchestra\Testbench\TestCase;
 
 class SoundsliceTest extends TestCase
 {
+    const TEST_FOLDER_ID = 5232;
+    const S3_DIR = 'soundslice-dev-1802';
 
+    private $folderId;
 
-    public function test_create()
+    /** @var $soundSliceService SoundsliceService */
+    protected $soundSliceService;
+
+    protected function setUp()
     {
-//        $this->assert
+        parent::setUp();
+
+        include __DIR__ . '../../../.env.testing';
+        if (empty(env('AWS_S3_SOUNDSLICE_ACCESS_KEY'))) { $this->fail("You must provide a value for the " .
+            "AWS_S3_SOUNDSLICE_ACCESS_KEY \'putenv' (environmental variable setting) function in `/.env.testing`.");}
+        if (empty(env('AWS_S3_SOUNDSLICE_ACCESS_SECRET'))) { $this->fail("You must provide a value for the " .
+            "AWS_S3_SOUNDSLICE_ACCESS_SECRET \'putenv' (environmental variable setting) function in `/.env.testing`.");}
+        if (empty(env('AWS_S3_SOUNDSLICE_REGION'))) { $this->fail("You must provide a value for the " .
+            "AWS_S3_SOUNDSLICE_REGION \'putenv' (environmental variable setting) function in `/.env.testing`.");}
+        if (empty(env('AWS_S3_SOUNDSLICE_BUCKET'))) { $this->fail("You must provide a value for the " .
+            "AWS_S3_SOUNDSLICE_BUCKET \'putenv' (environmental variable setting) function in `/.env.testing`.");}
+
+        $this->app['config']->set(
+            'railcontent.awsS3_soundslice',
+            [
+                'accessKey' => env('AWS_S3_SOUNDSLICE_ACCESS_KEY'),
+                'accessSecret' => env('AWS_S3_SOUNDSLICE_ACCESS_SECRET'),
+                'region' => env('AWS_S3_SOUNDSLICE_REGION'),
+                'bucket' => env('AWS_S3_SOUNDSLICE_BUCKET')
+            ]
+        );
+
+        $this->soundSliceService = $this->app->make(SoundsliceService::class);
     }
+
+
+    protected function tearDown()
+    {
+        if(isset($this->folderId)){
+            $this->deleteDummyFolder();
+        }
+
+        parent::tearDown();
+    }
+
+    private function createDummyFolder(){
+        $this->folderId = $this->soundSliceService->createFolder($this->getNameForADummy('folder'));
+    }
+
+    private function deleteDummyFolder(){
+        $success = $this->soundSliceService->deleteFolder($this->folderId);
+        if(!$success){
+            $this->fail('failed to delete dir created for this test (' . $this->folderId . ').');
+        }
+    }
+
+    private function getNameForADummy($specialTerm = ''){
+        if(!empty($specialTerm)){
+            $specialTerm = $specialTerm . '_';
+        }
+        return 'TEST_ ' . $specialTerm . 'SoundsliceServiceTest_' . time() . '_' . rand(000, 999);
+    }
+
+    // -----------------------------------------------------------------------------------------------------------------
+
+
 }
